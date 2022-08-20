291e602b18f9004f70aeda79a0f9ae648b314f59
pena1973 <penapisemnet@gmail.com>
2022-08-20 21:54:53 +0300

    Этап 2
    Добавила 3 страниц но никак не могу скрыть кнопку профиль из таба внизу, перерыла все свойства. видимо можно скрыть только весь таб целиком

--- App.js
+++ App.js
@@ -1,27 +1,30 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+// import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import 'react-native-gesture-handler';
 
 import Cats from './src/screens/cats';
 import Profile from './src/screens/profile';
+import Favorites from './src/screens/favorites';
 
-const Stack = createStackNavigator();
-
-function MyStack() {
+const Tab = createBottomTabNavigator();
+function MyTabs() {
   return (
-    <Stack.Navigator initialRouteName="Cats" screenOptions= {{headerShown: false}}>
-      <Stack.Screen name="Cats" component={Cats} />
-      <Stack.Screen name="Profile" component={Profile}/>
-    </Stack.Navigator>
-  );
-}
+    <Tab.Navigator initialRouteName="Cats" screenOptions={{ headerShown: false }} >
+      <Tab.Screen name="Cats" component={Cats} />
+      <Tab.Screen name="Profile" component={Profile} options = {{disableIconTint:false}}/> 
+      <Tab.Screen name="Favorites" component={Favorites} />
+    </Tab.Navigator>
+  )
+};
 
 export default function App() {
-  return (    
-     <NavigationContainer>
-       <MyStack />
-     </NavigationContainer>   
+  return (
+
+    <NavigationContainer>     
+      <MyTabs/>
+    </NavigationContainer>
+
   );
 }
-
---
+++ src/screens/favorites/index.js
@@ -0,0 +1,141 @@
+import React, { useEffect, useState } from 'react';
+import {
+  Text,
+  View,
+  StyleSheet,
+  FlatList,
+  Dimensions,
+  Image,
+  Platform,
+  TouchableOpacity,
+} from 'react-native';
+
+import axios from 'axios';
+axios.defaults.baseURL = 'https://api.thecatapi.com/v1/';
+axios.defaults.headers.common['x-api-key'] =
+  'f2899b7f-b575-4378-8ba4-7a4a9b1cabe0';
+
+const SCREEN_WIDTH = Dimensions.get('window').width;
+
+const deleteImageFromFavourites = async (favouritesId,loadFavorites) => {
+//  console.log(favouritesId);
+  const url = `favourites/${favouritesId}`;
+  try {
+    const response = await axios.delete(url);
+  //  console.log('response', response);
+    loadFavorites(); 
+  } catch (error) {
+  //  console.log(error);
+  } 
+};
+
+function ClientItem({ favorite,loadFavorites}) {
+  // console.log(favorite);
+  return (
+    <View style={styles.button}>
+      <Image style={styles.image1} source={{ uri: favorite?.image?.url }} />
+      <TouchableOpacity
+        style={{
+          position: 'absolute',
+          backgroundColor: '#ecf0f1',
+          width: 25,
+          padding: 5,
+          paddingLeft: 8,
+          borderRadius: 16,
+        }}
+        onPress={() => deleteImageFromFavourites(favorite?.id,loadFavorites)}>
+        <Text style={styles.buttonText}>X</Text>
+      </TouchableOpacity>
+    </View>
+  ); 
+}
+
+export default function Favorines() {
+  const [favorites, setFavorites] = useState();
+
+  const loadFavorites = () => {
+    const url = '/favourites?limit=10';
+    
+
+    axios
+      .get(url)
+      .then((response) => {
+        //console.log(response);
+        if (response?.data) {
+          setFavorites(response?.data);
+        }
+      })
+      .catch((error) => console.log(error));
+  };
+
+  useEffect(() => {
+    loadFavorites();
+    deleteImageFromFavourites();
+  }, []);
+
+  const renderItem = ({ item, index }) => {
+    return <ClientItem favorite={item} loadFavorites={loadFavorites()}  />;
+  };
+
+  return (
+    <View
+      style={{
+        resizeMode: 'contain',
+        flex: 1,
+        backgroundColor: '#ecf0f1',
+        marginTop: Platform.OS === 'android' ? 60 : 0,
+      }}>
+      <View style={styles.container}>
+        <FlatList data={favorites} renderItem={renderItem} />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ecf0f1',
+    padding: 8,
+    flexDirection: 'row',
+    borderRadius: 16,
+    resizeMode: 'contain',
+  },
+  button: {
+    flex: 1,
+    padding: 8,
+    flexDirection: 'row',
+    borderRadius: 16,
+    margin: 5,
+    backgroundColor: '#F7F7F7',
+    //  backgroundColor: 'grey',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 7,
+    },
+    shadowOpacity: 0.41,
+    shadowRadius: 9.11,
+    elevation: 14,
+  },
+  image1: {
+    height: 290,
+    width: 300,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 16,
+    flex: 1,
+
+    flexDirection: 'row',
+    backgroundColor: '#F7F7F7',
+
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 7,
+    },
+    shadowOpacity: 0.41,
+    shadowRadius: 9.11,
+    elevation: 14,
+  },
+});
